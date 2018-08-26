4e6aaaf4e1aaa35a0c421a03d15acc5eb0c78ce6
jfrazier <johnfrazier1222@gmail.com>
2018-08-26 13:56:58 -0400

    users/posts setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'page#home'
-  
+  resources :posts
+  root 'pages#home'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
